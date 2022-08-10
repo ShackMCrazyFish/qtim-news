b616ee6b16f516eb08704c4335e2fd68a918e1df
vitya <pepperforcemeat@outlook.com>
2022-08-10 22:57:06 +0300

    the news repository was added

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNewsDto } from './dto/create-news.dto';
 import { UpdateNewsDto } from './dto/update-news.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { News } from './entities/news.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class NewsService {
+  constructor(
+    @InjectRepository(News)
+    private newsRepository: Repository<News>,
+  ) {}
+
   create(createNewsDto: CreateNewsDto) {
-    return 'This action adds a new news';
+    return this.newsRepository.create(createNewsDto);
   }
 
-  findAll() {
-    return `This action returns all news`;
+  findAll(): Promise<News[]> {
+    return this.newsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} news`;
+  findOne(id: number): Promise<News> {
+    return this.newsRepository.findOneBy({ id });
   }
 
   update(id: number, updateNewsDto: UpdateNewsDto) {
-    return `This action updates a #${id} news`;
+    return this.newsRepository.update(id, updateNewsDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} news`;
+  async remove(id: number): Promise<void> {
+    await this.newsRepository.delete(id);
   }
 }
